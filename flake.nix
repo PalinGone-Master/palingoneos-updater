92051db0619ce7cf8859a848e435b8a52cb85236
PalinGone-Master <palingone@gmail.com>
2026-09-19 01:27:01 +0200

    Correction syntaxique unsafeDiscardReferences

--- flake.nix
+++ flake.nix
@@ -10,26 +10,25 @@
         pkgs = nixpkgs.legacyPackages.${system};
       in
       {
-        packages.palin-gone-os-updater = pkgs.stdenv.mkDerivation {
-      pname = "palin-gone-os-updater";
-      version = "1.1";
+       packages.palin-gone-os-updater = pkgs.stdenv.mkDerivation {
+         pname = "palin-gone-os-updater";
+         version = "1.1";
 
-      src = pkgs.fetchurl {
-        url = "https://github.com/PalinGone-Master/palingoneos-updater/releases/download/v1.1/palin-gone-os-updater";
-        hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+         src = pkgs.fetchurl {
+         url = "https://github.com/PalinGone-Master/palingoneos-updater/releases/download/v1.1/palin-gone-os-updater";
+         hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
       };
 
       dontUnpack = true;
 
-      # L'immunité diplomatique exigée par Nix :
       __structuredAttrs = true;
-      unsafeDiscardReferences.out = true;
+      unsafeDiscardReferences = { out = true; };
 
       installPhase = ''
         mkdir -p $out/bin
         cp $src $out/bin/palin-gone-os-updater
         chmod +x $out/bin/palin-gone-os-updater
-      '';
+      ''
     };
       }
     );
