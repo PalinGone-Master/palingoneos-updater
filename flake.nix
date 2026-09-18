6b62337f473cd063cf7676f08b1c5b21965f4327
PalinGone-Master <palingone@gmail.com>
2026-09-18 22:57:46 +0200

    fix: éradication définitive d'importCargoLock et correction de scope system

--- flake.nix
+++ flake.nix
@@ -1,46 +1,30 @@
 {
-  description = "PalinGoneOS Update";
+  description = "PalinGoneOS Updater";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      packages.${system} = {
-        palin-gone-os-updater = pkgs.rustPlatform.buildRustPackage {
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "palin-gone-os-updater";
           version = "1.0.0";
           src = ./.;
+
+          # L'unique et suprême méthode sans hachages
           cargoVendorDir = "vendor";
-          };
 
           nativeBuildInputs = [ pkgs.pkg-config ];
-          buildInputs = [ 
-            pkgs.wayland 
-            pkgs.libxkbcommon 
-          ];
+          buildInputs = [ pkgs.wayland pkgs.libxkbcommon ];
         };
-        default = self.packages.${system}.palin-gone-os-updater;
-      };
 
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          rustc
-          cargo
-          just
-          pkg-config
-          gcc
-          libxkbcommon.dev
-          wayland
-        ];
-
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-          pkgs.wayland
-          pkgs.libxkbcommon
-        ];
-     
-    };
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ cargo rustc rustfmt clippy ];
+        };
+      }
+    );
 }
