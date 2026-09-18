ac25ffd33f14b8821c8cb45de61e8328e1223f7b
PalinGone-Master <palingone@gmail.com>
2026-09-18 22:26:08 +0200

    fix: correction définitive du bloc packages

--- flake.nix
+++ flake.nix
@@ -9,20 +9,21 @@
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
-      packages.${system}.palin-gone-os-updater = pkgs.rustPlatform.buildRustPackage {
-        pname = "palin-gone-os-updater";
-        version = "1.0.0"; # Ajustez selon votre version
-        src = ./.;
-        cargoLock.lockFile = ./Cargo.lock;
+      packages.${system} = {
+        palin-gone-os-updater = pkgs.rustPlatform.buildRustPackage {
+          pname = "palin-gone-os-updater";
+          version = "1.0.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
 
-        nativeBuildInputs = [ pkgs.pkg-config ];
-        buildInputs = [ 
-          pkgs.wayland 
-          pkgs.libxkbcommon 
-        ];
+          nativeBuildInputs = [ pkgs.pkg-config ];
+          buildInputs = [ 
+            pkgs.wayland 
+            pkgs.libxkbcommon 
+          ];
+        };
+        default = self.packages.${system}.palin-gone-os-updater;
       };
-
-      packages.${system}.default = self.packages.${system}.palin-gone-os-updater;
 
       devShells.${system}.default = pkgs.mkShell {
         packages = with pkgs; [
